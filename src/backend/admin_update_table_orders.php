b03738e7faae67a4e1de4a769cf47a84c68738fc
AxdeExpe <s83799@htw-dresden.de>
2023-12-16 22:40:04 +0100

    finished admin_update_orders

--- src/backend/admin_update_table_orders.php
+++ src/backend/admin_update_table_orders.php
@@ -45,7 +45,7 @@
 
     if(!isset($_POST['order_id']) || empty($_POST['order_id']) || !isset($_POST['amount']) || !isset($_POST['price']) || !isset($_POST['order_date'])){
         # bad request, no book_id or order_id or title or amount or price or order_date
-        echo "bad request, no book_id or order_id or title or amount or price or order_date";
+        echo "bad request, no order_id or title or amount or price or order_date";
         http_response_code(400);
         exit;
     }
@@ -84,14 +84,14 @@
     }
 
     # check if order_date is valid
-    $date = DateTime::createFromFormat('Y-m-d H:i:s', $_POST['order_date']);
-    $errors = DateTime::getLastErrors();
+    $orderDate = $_POST['order_date'];
+    $date = DateTime::createFromFormat('Y-m-d H:i:s', $orderDate);
     
-    if (!$date || $errors['warning_count'] !== 0 || $errors['error_count'] !== 0) {
+    if ((!$date || $date->format('Y-m-d H:i:s') !== $orderDate) && !empty($_POST['order_date'])) {
         echo "Das Datum ist ungültig.";
         http_response_code(400);
         exit;
-    } 
+    }
 
 
     $host = "localhost";
@@ -113,11 +113,27 @@
     $selectStmt->bind_param("i", $_POST['order_id']);
     $selectStmt->execute();
     $result = $selectStmt->get_result();
+
+    if(!$result){
+        echo "Error while selecting the data!";
+        http_response_code(500);
+        exit;
+    }
+
+    if($result && $result->num_rows === 0){
+        echo "No data found!";
+        http_response_code(404);
+        exit;
+    }
+
     $existingData = $result->fetch_assoc();
     $selectStmt->close();
 
+    echo intval($_POST['amount']);
+
     if (
-        (empty($_POST['amount']) || $existingData['amount'] == $_POST['amount']) &&
+        ($_POST['amount'] == 0 && !empty($_POST['amount'])) ||
+        ($_POST['amount'] != 0 && (empty($_POST['amount']) || $existingData['amount'] == $_POST['amount'])) &&
         (empty($_POST['price']) || $existingData['price'] == $_POST['price']) &&
         (empty($_POST['order_date']) || $existingData['order_date'] == $_POST['order_date'])
     ) {
@@ -125,7 +141,110 @@
         http_response_code(304);
         exit;
     }
+    
+    # delete if amount is 0
+    if ($_POST['amount'] == 0) {
+        $sql = "DELETE FROM orders WHERE order_id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $_POST['order_id']);
+        $stmt->execute();
+        $conn->close();
+        echo "Successfully deleted";
+        http_response_code(200);
+        exit;
+    }
+    
 
+    if(empty($_POST['amount'])){
+        $selectAmount = "SELECT amount FROM orders WHERE order_id = ?";
+        $selectAmountStmt = $conn->prepare($selectAmount);
+        $selectAmountStmt->bind_param("i", $_POST['order_id']);
+
+        if(!$selectAmountStmt){
+            echo "Error while preparing the statement!";
+            http_response_code(500);
+            exit;
+        }
+
+        $selectAmountStmt->execute();
+        $result = $selectAmountStmt->get_result();
+
+        if(!$result){
+            echo "Error while selecting the data!";
+            http_response_code(500);
+            exit;
+        }
+
+        $amount = $result->fetch_assoc()['amount'];
+        $selectAmountStmt->close();
+
+        $_POST['amount'] = $amount;
+    }
+
+
+    # get the price of the book
+    $selectBookID = "SELECT book_id, amount FROM orders WHERE order_id = ?";
+    $selectBookIDStmt = $conn->prepare($selectBookID);
+
+    if(!$selectBookIDStmt){
+        echo "Error while preparing the statement!";
+        http_response_code(500);
+        exit;
+    }
+
+    $selectBookIDStmt->bind_param("i", $_POST['order_id']);
+    $selectBookIDStmt->execute();
+
+    $result = $selectBookIDStmt->get_result();
+
+    if(!$result){
+        echo "Error while selecting the data!";
+        http_response_code(500);
+        exit;
+    }
+
+    $row = $result->fetch_assoc();
+
+    $book_id = $row['book_id'];
+    $old_amount = $row['amount'];
+    $selectBookIDStmt->close();
+
+    $selectPrice = "SELECT price_brutto, stock FROM books WHERE id = ?";
+    $selectPriceStmt = $conn->prepare($selectPrice);
+
+    if(!$selectPriceStmt){
+        echo "Error while preparing the statement!";
+        http_response_code(500);
+        exit;
+    }
+
+    $selectPriceStmt->bind_param("i", $book_id);
+    $selectPriceStmt->execute();
+
+    $result = $selectPriceStmt->get_result();
+
+    if(!$result){
+        echo "Error while selecting the data!";
+        http_response_code(500);
+        exit;
+    }
+
+    $row = $result->fetch_assoc();
+    $price_brutto = $row['price_brutto'];
+    $stock = $row['stock'];
+
+    $selectPriceStmt->close();
+
+
+    # 1 >= 1 + (9 - 19)
+    if(($stock + intval($old_amount)) < intval($_POST['amount'])){
+        echo "The stock is smaller than the amount";
+        http_response_code(400);
+        exit;
+    }
+
+
+    # update the order
 
     $sql = "UPDATE orders SET
     price = IF(?, IF(LENGTH(?) > 0, ?, price), price),
@@ -141,11 +260,25 @@
     exit;
     }
 
-    $stmt->bind_param("dddiiisssi", 
-    $_POST['price'], $_POST['price'], $_POST['price'],
-    $_POST['amount'], $_POST['amount'], $_POST['amount'],
-    $_POST['order_date'], $_POST['order_date'], $_POST['order_date'],
-    $_POST['order_id']);
+    $priceCalc = floatval($_POST['amount']) * $price_brutto;
+
+
+    echo $priceCalc . " " . $price_brutto . " " . $_POST['amount'];
+
+    if(empty($_POST['price'])){
+        $stmt->bind_param("dddiiissi", 
+        $priceCalc, $priceCalc, $priceCalc,
+        $_POST['amount'], $_POST['amount'], $_POST['amount'],
+        $_POST['order_date'], $_POST['order_date'],
+        $_POST['order_id']);
+    }
+    else{
+        $stmt->bind_param("dddiiissi", 
+        $_POST['price'], $_POST['price'], $_POST['price'],
+        $_POST['amount'], $_POST['amount'], $_POST['amount'],
+        $_POST['order_date'], $_POST['order_date'],
+        $_POST['order_id']);
+    }
 
 
     $stmt->execute();
@@ -155,8 +288,6 @@
     echo $stmt->get_result();
 
     echo $stmt->error;
-
-    echo $_POST['image'] . " " . $_POST['title'] . " " . $_POST['author'] . " " . $_POST['price_netto'] . " " . $_POST['mwst'] . " " . $_POST['weight'] . " " . $_POST['stock'] . " " . $_POST['description'] . " " . $_POST['publisher'] . " " . $_POST['id'];
 
     if ($stmt->affected_rows > 0) {
         echo "successfull updated";
