9397c4bfc3fd37bc947e2020b45984729660eb6c
AxdeExpe <s83799@htw-dresden.de>
2023-12-19 18:59:43 +0100

    add frontend

---
+++ src/frontend/src/router/index.ts
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginButtonVue from '../components/LoginButton.vue'
+import Katalog from '../components/KatalogPage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/katalog',
+      name: 'katalog',
+      // component: () => import('../views/KatalogView.vue')
+      component: Katalog
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // component: () => import('../views/Login-Button.vue')
+      component: LoginButtonVue
+    }
+  ]
+});
+
+export default router;
