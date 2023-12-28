33ce955a58cfdb264ca67020fd8c7df0ac5f967b
Kevin Pietzsch <s83799@htw-dresden.de>
2023-12-28 15:04:22 +0200

    add stripe succes db handling

--- src/backend/database/orders.sql
+++ src/backend/database/orders.sql
@@ -4,6 +4,12 @@ CREATE TABLE Orders (
     order_date TIMESTAMP NOT NULL,
     amount INT NOT NULL,
     price DECIMAL NOT NULL,
+
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    stripe_checkout_session_id VARCHAR(100) DEFAULT NULL,
+    txn_id VARCHAR(50) NOT NULL,
+    customer_name VARCHAR(50) NOT NULL,
+    customer_email VARCHAR(50) NOT NULL;
     
     PRIMARY KEY(order_id),
     FOREIGN KEY(book_id) REFERENCES Books(id)
--- src/backend/stripe/success.php
+++ src/backend/stripe/success.php
@@ -1,6 +1,7 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
+require('./stripe-php-master/init.php');
 
 if($_SERVER['REQUEST_METHOD'] !== 'GET'){
     # no get request
@@ -136,8 +137,37 @@ if (!$conn->set_charset("utf8mb4")) {
 
 
 
+# check if transaction already exists
+
+$statement = $conn->prepare("SELECT * FROM Orders WHERE stripe_checkout_session_id = ?");
+
+if(!$statement){
+    http_response_code(500);
+    exit;
+}
+
+$statement->bind_param("s", $_GET['session_id']);
+$statement->execute();
+$result = $statement->get_result();
+
+$statement->close();
+
+if($result->num_rows > 0){
+    $conn->close();
+    echo "Transaction already exists!";
+    http_response_code(409);
+    exit;
+}
+
+
 for($j = 1; $j <= $numberOfArrays; $j++){
     $statement = $conn->prepare("SELECT * FROM Books WHERE id = ?");
+    
+    if(!$statement){
+        http_response_code(500);
+        exit;
+    }
+
     $statement->bind_param("i", $jsonData[$j]['id']);
     $statement->execute();
     $result = $statement->get_result();
@@ -162,11 +192,146 @@ for($j = 1; $j <= $numberOfArrays; $j++){
 
 
 
+// Set API key 
+$stripe = new \Stripe\StripeClient(STRIPE_API_KEY); 
+         
+// Fetch the Checkout Session to display the JSON result on the success page 
+$checkout_session = null;
+$api_error = null;
 
+try { 
+    $checkout_session = $stripe->checkout->sessions->retrieve($session_id); 
+} catch(Exception $e) {  
+    $api_error = $e->getMessage();  
+    echo $api_error;
+    http_response_code(500);
+    exit;
+} 
+
+if($api_error !== null || $checkout_session === null){
+    echo "Invalid transaction!";
+    http_response_code(500);
+    exit;
+}
+
+$customer_details = $checkout_session->customer_details;
+$paymentIntent = null;
+
+try { 
+    $paymentIntent = $stripe->paymentIntents->retrieve($checkout_session->payment_intent); 
+} catch (\Stripe\Exception\ApiErrorException $e) { 
+    $api_error = $e->getMessage(); 
+    echo $api_error;
+    http_response_code(500);
+    exit;
+}
+
+if($api_error !== null || $paymentIntent === null){
+    echo "Unable to fetch the transaction details!";
+    http_response_code(500);
+    exit;
+}
+
+if($paymentIntent === null || $paymentIntent->status !== 'succeeded'){
+    echo "Transaction has been failed!";
+    http_response_code(500);
+    exit;
+}
+
+$transactionID = $paymentIntent->id; 
+$paidAmount = $paymentIntent->amount; 
+$paidAmount = ($paidAmount/100); 
+$paidCurrency = $paymentIntent->currency; 
+$payment_status = $paymentIntent->status; 
+                     
+// Customer info 
+$customer_name = $customer_email = ''; 
+
+if(!empty($customer_details)){ 
+    $customer_name = !empty($customer_details->name) ? $customer_details->name:''; 
+    $customer_email = !empty($customer_details->email) ? $customer_details->email:''; 
+}
+
+
+// Check if any order data is exists already with the same TXN ID
+$statement = $conn->prepare("SELECT * FROM Orders WHERE txn_id = ?");
+
+if(!$statement){
+    http_response_code(500);
+    exit;
+}
+
+$statement->bind_param("s", $transactionID);
+$statement->execute();
+$result = $statement->get_result();
+$prevRow = $result->fetch_assoc()
+
+$statement->close();
 
 # insert into the purchases into Orders
 $conn->begin_transaction();
 
+for($i = 1; i <= $numberOfArrays; $i++){
+
+    # need to be changed ##########################################################################################################################################################################################################
+    // Insert transaction data into the database 
+    $sql = "INSERT INTO Orders (customer_name,customer_email,item_name,item_number,item_price,item_price_currency,paid_amount,paid_amount_currency,txn_id,payment_status,stripe_checkout_session_id,created,modified) VALUES (?,?,?,?,?,?,?,?,?,?,?,NOW(),NOW())"; 
+    $stmt = $db->prepare($sql); 
+        
+    if(!$stmt){
+        http_response_code(500);
+        exit;
+    }
+        
+    $stmt->bind_param("ssssdsdssss", $customer_name, $customer_email, $productName, $productID, $productPrice, $currency, $paidAmount, $paidCurrency, $transactionID, $payment_status, $session_id); 
+    $stmt->execute(); 
+        
+    if($stmt->affected_rows <= 0){
+        $conn->rollback();
+        $conn->close();
+        http_response_code(500);
+        exit;
+    }
+
+    $statement->close();
+}
+
+$conn->commit();
+
+for($i = 1; i <= $numberOfArrays; $i++){
+
+    // Führe die Aktualisierung der Books-Tabelle aus
+    $updateSql = "UPDATE Books SET stock = stock - ? WHERE id = ?";
+    $updateStmt = $conn->prepare($updateSql);
+
+    if (!$updateStmt) {
+        http_response_code(500);
+        exit;
+    }
+
+    $updateStmt->bind_param("ii", $jsonData[$i]['amount'], $json_data[$i]['id']);
+    $updateStmt->execute();
+
+    if($updateStmt->affected_rows <= 0){
+        $conn->rollback();
+        $conn->close();
+        http_response_code(500);
+        exit;
+    }
+}
+
+$conn->commit();
+$conmn->close();
+
+http_response_code(200);
+exit;
+
+# redirect to called url
+# echo "<script>setTimeout(\"location.href = 'http://ivm108.informatik.htw-dresden.de/ewa/g08/frontend/index.php';\",1500);</script>";
+
+
+
+/*
 for($i = 1; i <= $numberOfArrays; $i++){
 
     $sql = "INSERT INTO Orders (book_id, order_date, amount, price)
@@ -201,38 +366,10 @@ for($i = 1; i <= $numberOfArrays; $i++){
 
 $conn->commit();
 
+*/
 
 
-for($i = 1; i <= $numberOfArrays; $i++){
 
-    // Führe die Aktualisierung der Books-Tabelle aus
-    $updateSql = "UPDATE Books SET stock = stock - ? WHERE id = ?";
-    $updateStmt = $conn->prepare($updateSql);
-
-    if (!$updateStmt) {
-        http_response_code(500);
-        exit;
-    }
-
-    $updateStmt->bind_param("ii", $jsonData[$i]['amount'], $json_data[$i]['id']);
-    $updateStmt->execute();
-
-    if($updateStmt->affected_rows <= 0){
-        $conn->rollback();
-        $conn->close();
-        http_response_code(500);
-        exit;
-    }
-}
-
-$conn->commit();
-$conmn->close();
-
-http_response_code(200);
-exit;
-
-# redirect to called url
-# echo "<script>setTimeout(\"location.href = 'http://ivm108.informatik.htw-dresden.de/ewa/g08/frontend/index.php';\",1500);</script>";
 
 ?>
 
