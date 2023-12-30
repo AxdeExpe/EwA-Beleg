b960ca06f34a023b5537b07908a6fc581b9e38f4
Kevin Pietzsch <s83799@htw-dresden.de>
2023-12-31 00:17:01 +0200

    add first steps for the order

--- src/frontend/src/components/Einkauf/WarenkorbPage.vue
+++ src/frontend/src/components/Einkauf/WarenkorbPage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { ref } from "vue";
-import { store, removeFromWarenkorb, updateGesamtsumme, isloggedIn } from '@/store';
+import { store, removeFromWarenkorb, updateGesamtsumme, isloggedIn, username, password } from '@/store';
 import router from '@/router';
 
 const warenkorb = store.value.warenkorb;
@@ -16,19 +16,97 @@ const gesamtsumme = () => {
   }, 0).toFixed(2);
 };
 
-let checkLogin = () => {
+let order = async () => {
   console.log(isloggedIn.value);
 
   if(isloggedIn.value){
     //bestellen
     console.log('bestellen');
 
+
+
+    // Dynamicly wrap JSON-Datapacket
+    let data = [];
+
+
+    // TODO get username and password from store, currently it is undefined
+    data.push({
+      "username": username,
+      "password": password
+    });
+
+    // get the book id and the amount of books
+    for(let i = 0; i < store.value.warenkorb.length; i++){
+      data.push({
+        "id": store.value.warenkorb[i].id,
+        "amount": store.value.warenkorb[i].quantity
+      });
+    }
+
+    console.log(data);
+    return;
+
+
     //request zum server und mit stripe bezahlen
-  }
+    try{
+      let response = await fetch('http://ivm108.informatik.htw-dresden.de/ewa/g08/backend/stripe/stripe.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+
+        // JSON-Format:
+        // [
+        //     {"username": "test_user", "password": "test"},
+        //     {"id": 1, "amount": 2},
+        //     {"id": 2, "amount": 7}
+        // ]
+
+
+        body: JSON.stringify({
+          "username": username,
+          "password": password,
+          "warenkorb": store.value.warenkorb
+        })
+
+
+
+      });
+
+      if (response.status === 200) {
+        let data = await response.json();
+
+
+
+
+
+      } else if (response.status === 400) {
+        console.error('warenkorb is empty or not set, no POST-Request');
+        alert('warenkorb is empty or not set, no POST-Request');
+      } else if (response.status === 401) {
+        console.error('not authorized');
+        alert('not authorized');
+      } else if(response.status === 404){
+        console.error('Any book not found');
+        alert('Any book not found');
+      } else if(response.status === 409){
+        console.error('Not enough books in stock');
+        alert('Not enough books in stock');
+      } else if (response.status === 500) {
+        console.error('Server Error');
+        alert('Server Error');
+      }
+    }
+    catch (e) {
+      console.log(e);
+    }
+
+    }
   else{
     //login
     alert('Um bestellen zu können, müssen Sie sich einloggen!');
-
+    return;
   }
 
 };
@@ -50,7 +128,7 @@ let checkLogin = () => {
       <strong>Gesamtsumme: {{ gesamtsumme() }}€</strong>
     </div>
 
-    <button @click="checkLogin()">Bestellen</button>
+    <button @click="order()">Bestellen</button>
   </div>
 </template>
 
