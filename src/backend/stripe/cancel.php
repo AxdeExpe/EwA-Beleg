2715cd74f3cc92e13f2d04d8ee121b46a5315e22
Kevin Pietzsch <s83799@htw-dresden.de>
2024-01-02 21:15:36 +0200

    improved cancel page

--- src/backend/stripe/cancel.php
+++ src/backend/stripe/cancel.php
@@ -8,14 +8,75 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Abgebrochene Bestellung</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        form {
+            background-color: #2196F3;
+            border: 2px solid #ccc;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            max-width: 400px;
+            width: 100%;
+            margin: auto;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+            font-weight: bold;
+        }
+
+        input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 16px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        button {
+            background-color: #4CAF50;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+
+    </style>
 </head>
 <body>
-    <h1>Der Bezhalvorgang wurde abgebrochen!</h1>
-    <h3>Sie werden in Kürze weitergeleitet!</h3>
+
+        <form>
+            <h2>Bestellung abgebrochen</h2>
+            <p>Leider wurde Ihre Bestellung abgebrochen</p>
+            <p>Kontaktieren Sie unseren Kunden-Support, falls Sie Fragen oder Schwierigkeiten haben.</p>
+
+            <img src="./cancel.png" alt="Canceld order">
+        </form>
+
+
 </body>
 </html>
